75ebff66a79c0825e4ed25406fbf777e503e4cf0
eNnillaMS <hboutilier1996@hotmail.com>
2014-03-15 12:33:22 -0700

    03152014-3
    Made the page look better, changed the TODTheme system to work without
    CSS files because whynot. Next update is going to be all for the JS.  :)

--- script/ToDThemes.js
+++ script/ToDThemes.js
@@ -1,8 +1,17 @@
 //Simple Time of Day script to determine which theme I want to load.
-var day = new Date();
-var hour = day.getHours();
-var e = document.getElementById("_theme");
-if (hour >= 21 || hour < 3) e.href = 'themes/Night.css';
-else if (hour >= 3 && hour < 9) e.href = 'themes/Morning.css';
-else if (hour >= 9 && hour < 15) e.href = 'themes/Day.css';
-else if (hour >= 15 && hour < 21) e.href = 'themes/Dusk.css';
+var hour = new Date().getHours(), img, color;
+if (hour >= 21 || hour < 3){
+	img = 'themes/TOD/Night.jpg';
+} else if (hour >= 3 && hour < 9){
+	img = 'themes/TOD/Dawn.jpg';
+} else if (hour >= 9 && hour < 15){
+	img = 'themes/TOD/Day.jpg';
+} else if (hour >= 15 && hour < 21){
+	img = 'themes/TOD/Dusk.jpg';
+}
+var i = new Image();
+i.onload = function(){
+	var e = document.body;
+	e.style.backgroundImage = 'url("' + img + '")';
+};
+i.src = img;
