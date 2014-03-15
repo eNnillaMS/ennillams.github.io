094f7f12f7696299a93c5b39d1741545e325fd3d
eNnillaMS <hboutilier1996@hotmail.com>
2014-03-15 10:48:59 -0700

    03152014-1
    Fixed links in files. DOH.

--- script/ToDThemes.js
+++ script/ToDThemes.js
@@ -2,7 +2,7 @@
 var day = new Date();
 var hour = day.getHours();
 var e = document.getElementById("_theme");
-if (hour >= 21 || hour < 3) e.href = '_themes/Night.css';
-else if (hour >= 3 && hour < 9) e.href = '_themes/Morning.css';
-else if (hour >= 9 && hour < 15) e.href = '_themes/Day.css';
-else if (hour >= 15 && hour < 21) e.href = '_themes/Dusk.css';
+if (hour >= 21 || hour < 3) e.href = 'themes/Night.css';
+else if (hour >= 3 && hour < 9) e.href = 'themes/Morning.css';
+else if (hour >= 9 && hour < 15) e.href = 'themes/Day.css';
+else if (hour >= 15 && hour < 21) e.href = 'themes/Dusk.css';
